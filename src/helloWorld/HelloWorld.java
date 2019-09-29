618226abbfa4297930edc95e180143664c2cb9cc
Ester <Ester@macbook-pro-2.home>
2019-09-29 20:08:07 +0200

    añado ejercicio HelloWorld

---
+++ src/helloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+package helloWorld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.print("Hello world!");
+		System.out.println("Al final de mi frase meto un salto de línea.");
+		System.out.print("Esto se imprime en una línea nueva.");
+}
+}
